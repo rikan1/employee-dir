6879e6e64f9baaca9cf4b5cd0fb9f29ca63d3c74
somyahubilo <somyaranjan@hubilo.com>
2021-12-28 23:14:58 +0530

    project set up

---
+++ models/users.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+var UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: true
+  },
+  email: {
+    type: String,
+    trim: true,
+    required: true,
+    // unique: true
+  },
+  phone: {
+    type: String,
+    trim: true,
+    required: true,
+    // unique: true
+  },
+  companyId: {
+    type: String,
+    trim: true,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  }
+});
+
+UserSchema.pre("save", function (next) {
+  var user = this;
+  if (user.isModified("password")) {
+    bcrypt.genSalt(5, (err, salt) => {
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+});
+
+var User = mongoose.model("Users", UserSchema);
+
+module.exports = { User };
